2cb7c1f41a385ee57fd4d2a791316c2e8ede7d69
rafizul896 <rafizulislam896@gmail.com>
2024-11-07 14:53:05 +0600

    fix: problem-3

--- dist/problem-3.js
+++ dist/problem-3.js
@@ -1,9 +1,10 @@
 "use strict";
 {
     const countWordOccurrences = (sentence, word) => {
-        const wordArr = sentence.toLowerCase().split(/\W+/);
+        const wordArr = sentence.toLowerCase().split(' ');
         const targetWord = word.toLowerCase();
         const total = wordArr.filter(w => w === targetWord).length;
         return total;
     };
+    console.log(countWordOccurrences("I love typescript", "typescript"));
 }
